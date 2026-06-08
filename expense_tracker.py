4e2d236ab94576191c2f760068718dd03772e0ea
Keshav Jain <jainkeshav654@gmail.com>
2026-06-08 12:59:56 +0530

    Add menu driven interface

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,2 +1,16 @@
 def main():
-    pass
+    
+    while True:
+        print("\n===== Expense Tracker =====")
+        print("1. Add Expense")
+        print("2. View Expenses")
+        print("3. Filter By Category")
+        print("4. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "4":
+            break
+
+if __name__ == "__main__":
+    main()
