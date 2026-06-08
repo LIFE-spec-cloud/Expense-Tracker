e332d7a7d5a5f292e7d8aa4e9f45e530aa84a381
Keshav Jain <jainkeshav654@gmail.com>
2026-06-08 13:05:15 +0530

    Add functionality to record expenses

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 DATA_FILE = "expenses.json"
 
@@ -14,7 +15,50 @@ def save_expenses(expenses):
     with open(DATA_FILE, "w") as file:
         json.dump(expenses, file, indent=4)
         
-        
+def add_expense():
+
+    try:
+        amount = float(input("Enter amount: "))
+    except ValueError:
+        print("Invalid amount.")
+        return
+
+    print("\nCategories:")
+    print("Food")
+    print("Transport")
+    print("Entertainment")
+    print("Other")
+
+    category = input("Enter category: ").strip()
+
+    valid_categories = [
+        "Food",
+        "Transport",
+        "Entertainment",
+        "Other"
+    ]
+
+    if category not in valid_categories:
+        print("Invalid category.")
+        return
+
+    note = input("Enter note (optional): ")
+
+    date = datetime.now().strftime("%Y-%m-%d")
+
+    expense = {
+        "date": date,
+        "category": category,
+        "amount": amount,
+        "note": note
+    }
+
+    expenses = load_expenses()
+    expenses.append(expense)
+    save_expenses(expenses)
+
+    print("Expense added successfully!")
+
 def main():
     
     while True:
